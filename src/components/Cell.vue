510d3adbc4019c8035c5b29f4bcf75bab888a667
sefirus <sashasam348@gmail.com>
2023-06-20 23:10:07 +0300

    Changed styling

--- src/components/Cell.vue
+++ src/components/Cell.vue
@@ -37,11 +37,15 @@ export default {
   text-align: center;
   background-color: #ffeb95;
   color: #3f1f4b;
+  border-radius: 15px; /* Add rounded corners */
+  box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2); /* Add box-shadow for 3D effect */
+  transition: all 0.1s ease-in-out; /* Add transition for smooth movement */
 }
 
 .empty {
   background-color: #3f1f4b;
   cursor: not-allowed;
   color: transparent;
+  box-shadow: none; /* Remove box-shadow from the empty cell */
 }
 </style>
